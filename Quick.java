6006b7b417d5379c84602b4305bbade9911060e0
Qiong <qhuang00@stuy.edu>
2019-03-12 13:18:26 -0400

    made optimization 2 to partition, problme with quick select

--- Quick.java
+++ Quick.java
@@ -10,23 +10,23 @@ public class Quick{
    Arrays.sort(pivotPossibilities);
    int pivotIndex = pivotPossibilities[1]; // chooses random pivot index
    int pivot = data[pivotIndex]; //sets the pivot
-   int pointer = start + 1; //points to value right after start
+   int pointer = start; //points to value right after start
    data[pivotIndex] = data[start]; //moves pivot to front
    data[start] = pivot;
    //System.out.println(pivot);
    for (int x = start; x < end ; x++ ) {
      //System.out.println(Arrays.toString(data));
      //System.out.println(pointer);
-     if(data[x] < pivot){ //swaps value at pointer and moves pivot if below
+     if(data[x] < pivot  || (data[x] == pivot && Math.random() > 0.5) ){ //swaps value at pointer and moves pivot if below
+       pointer++; //moves pointer up
        int temp = data[pointer];
        data[pointer] = data[x];
        data[x] = temp;
-       pointer++; //moves pointer up
      }
    }
-   data[start] = data[pointer-1]; //moves pivot to right location based on pointer (values left of pointer are below pivot)
-   data[pointer -1] = pivot;
-   return pointer -1 ;
+   data[start] = data[pointer]; //moves pivot to right location based on pointer (values left of pointer are below pivot)
+   data[pointer] = pivot;
+   return pointer ;
  }
 
  public static int quickselect(int []data, int k){
@@ -48,7 +48,9 @@ public class Quick{
    return data[k];
  }
  public static void main(String[] args) {
-   int[] numbers = {765,564,123,212,324,56,76,8,123,34};
-   System.out.println(quickselect(numbers, 4));
+   int[] numbers = {765,564,123,212,324,56,76,8,123,123,123,34};
+   System.out.println(partition(numbers,0,numbers.length));
+   System.out.println(Arrays.toString(numbers));
+  System.out.println(quickselect(numbers, 4));
  }
 }
